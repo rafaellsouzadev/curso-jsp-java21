59d1af18ca1b778c541fe0a29bcb999af832bce4
Rafael de Souza Alves <rafaellsouza.java@gmail.com>
2025-04-18 21:38:02 -0300

    Iniciando o projeto do curso

---
+++ curso-jsp-java21/src/main/java/servlets/ServletLogin.java
@@ -0,0 +1,32 @@
+package servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ServletLogin extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public ServletLogin() {
+	
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}
